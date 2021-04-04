18b80095ac53e3c592e5765fa8f4ff3888efe118
Jason Jones <JsunJ.Jones@gmail.com>
2021-04-04 17:39:12 -0400

    added internationalization and language choice

--- lesson-2-small-programs/13-assignment-calculator-bonus-features/calculator.js
+++ lesson-2-small-programs/13-assignment-calculator-bonus-features/calculator.js
@@ -1,3 +1,5 @@
+// Ask the user for their preferred language.
+// Check to see if the input is a valid language option.
 // Ask the user for the first number.
 // Check to see if the input is a valid number.
 // Ask the user for the second number.
@@ -6,10 +8,19 @@
 // Check to see if the input is a valid operation.
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
+// Ask the user if they would like to continue.
+// Check to see if the input is valid.
+// Loop back to line 3 if the user answered yes.
 
 const readline = require('readline-sync');
 
 const MESSAGES = require('./calculator_messages.json');
+
+let language = 'en';
+
+function messages(languageKey, message) {
+  return MESSAGES[languageKey][message];
+}
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
@@ -24,27 +35,27 @@ let number2;
 let operation;
 
 function getInputs() {
-  prompt(MESSAGES.firstNumber);
+  prompt(messages(language, 'firstNumber'));
   number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt(MESSAGES.validNumber);
+    prompt(messages(language, 'validNumber'));
     number1 = readline.question();
   }
 
-  prompt(MESSAGES.secondNumber);
+  prompt(messages(language, 'secondNumber'));
   number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt(MESSAGES.validNumber);
+    prompt(messages(language, 'validNumber'));
     number2 = readline.question();
   }
 
-  prompt(MESSAGES.operation);
+  prompt(messages(language, 'operation'));
   operation = readline.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(MESSAGES.validOperation);
+    prompt(messages(language, 'validOperation'));
     operation = readline.question();
   }
 }
@@ -66,7 +77,7 @@ function findResult() {
       output = Number(number1) / Number(number2);
       break;
   }
-  prompt(MESSAGES.result + ` ${output}.`);
+  prompt(messages(language, 'result') + ` ${output}.`);
 }
 
 let again;
@@ -75,19 +86,45 @@ function runCalculator() {
   getInputs();
   findResult();
 
-  prompt(MESSAGES.again);
+  prompt(messages(language, 'again'));
   again = readline.question();
 
   while (!['1', '2'].includes(again)) {
-    prompt(MESSAGES.validAgain);
+    prompt(messages(language, 'validAgain'));
     again = readline.question();
   }
 
   if (again === '1') {
+    console.clear();
     runCalculator();
   }
 }
 
-prompt(MESSAGES.welcome);
+// Set language for calculator.
+let languageChoice;
+
+prompt('1) English 2) Español 3) Français');
+languageChoice = readline.question();
+
+while (!['1', '2', '3'].includes(languageChoice)) {
+  prompt('1) English 2) Español 3) Français');
+  languageChoice = readline.question();
+}
+
+switch (languageChoice) {
+  case '1':
+    language = 'en';
+    break;
+  case '2':
+    language = 'es';
+    break;
+  case '3':
+    language = 'fr';
+    break;
+}
+
+
+// Begin calculator.
+prompt(messages(language, 'welcome'));
 
 runCalculator();
