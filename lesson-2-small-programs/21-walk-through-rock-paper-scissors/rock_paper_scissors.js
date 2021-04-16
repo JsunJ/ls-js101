b29b3c61eeb7a36281a1050589c05d0d0d10bf94
Jason Jones <JsunJ.Jones@gmail.com>
2021-04-15 20:41:36 -0400

    finished logic, refactored code

--- lesson-2-small-programs/21-walk-through-rock-paper-scissors/rock_paper_scissors.js
+++ lesson-2-small-programs/21-walk-through-rock-paper-scissors/rock_paper_scissors.js
@@ -5,39 +5,64 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-while (true) {
+function getUserChoice() {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
+  let userChoice = readline.question().toLowerCase();
 
-  while (!VALID_CHOICES.includes(choice)) {
+  while (!VALID_CHOICES.includes(userChoice)) {
     prompt('That is not a valid choice.');
-    choice = readline.question();
+    userChoice = readline.question().toLowerCase();
   }
+  return userChoice;
+}
 
+function getComputerChoice() {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
+  return computerChoice;
+}
 
-  prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
+function displayWinner() {
+  let userChoice = getUserChoice();
+  let computerChoice = getComputerChoice();
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'scissors' && computerChoice === 'paper') ||
-      (choice === 'paper' && computerChoice === 'rock')) {
+  prompt(`You chose ${userChoice}.`);
+  prompt(`Computer chose ${computerChoice}.`);
+
+  if ((userChoice === 'rock' && computerChoice === 'scissors') ||
+      (userChoice === 'scissors' && computerChoice === 'paper') ||
+      (userChoice === 'paper' && computerChoice === 'rock')) {
     prompt('You win!');
-  } else if ((choice === 'scissors' && computerChoice === 'rock') ||
-  (choice === 'paper' && computerChoice === 'scissors') ||
-  (choice === 'rock' && computerChoice === 'paper')) {
+  } else if ((userChoice === 'scissors' && computerChoice === 'rock') ||
+              (userChoice === 'paper' && computerChoice === 'scissors') ||
+              (userChoice === 'rock' && computerChoice === 'paper')) {
     prompt('Computer wins!');
   } else {
     prompt("It's a tie!");
   }
+}
 
-  prompt('Would you like to play again? (Y/N)');
+function repeat() {
+  prompt('Would you like to play again? (Y / N)');
   let answer = readline.question().toLowerCase();
 
-  while (answer[0] !== 'y' && answer[0] !== 'n') {
+  while (answer !== 'y' && answer !== 'n') {
     prompt("Please enter 'y' or 'n'.");
     answer = readline.question().toLowerCase();
   }
 
-  if (answer[0] !== 'y') break;
+  if (answer === 'y') {
+    console.clear();
+    runGame();
+  } else {
+    prompt('Exiting game.');
+  }
 }
+
+function runGame() {
+  displayWinner();
+  repeat();
+}
+
+prompt('Welcome to Rock Paper Scissors!');
+runGame();
